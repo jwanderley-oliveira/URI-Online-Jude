0cf420ff1f146a1360200f278bd8d11025989df5
wanderley <jwanderley.oliveira@outlook.com>
2020-05-13 15:33:00 -0300

    Adicionado URI_1003

---
+++ 1003_uri_simple_sum.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace _1003_uri_simple_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int A,B, soma;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+
+            Console.WriteLine("SOMA = " + soma);
+
+            Console.ReadLine();            
+
+        }
+    }
+}
