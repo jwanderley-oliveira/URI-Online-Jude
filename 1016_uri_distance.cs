82882db169ae318d011d4e0d4298e305a9783644
wanderley <jwanderley.oliveira@outlook.com>
2020-05-13 15:51:17 -0300

    Adicionado URI_1016

---
+++ 1016_uri_distance.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace _1016_uri_distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int distance, time;
+
+            distance = int.Parse(Console.ReadLine());
+
+            time = distance * 2;
+
+            Console.WriteLine(time + " minutos");
+
+            Console.ReadLine();
+
+        }
+    }
+}
